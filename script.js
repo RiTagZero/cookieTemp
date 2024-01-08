4f8ea31c587dc433c7e0efbd9c890dbee74b86cc
RiTagZero <92667869+RiTagZero@users.noreply.github.com>
2024-01-08 19:43:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+// script.js
+
+// Function to get a cookie
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+// Function to load preferences
+function loadPreferences() {
+    var urlParams = new URLSearchParams(window.location.search);
+    var isGuest = urlParams.get('guest');
+
+    if (isGuest) {
+        document.body.style.backgroundColor = 'gray';
+        document.getElementById("welcomeMessage").textContent = "Laipni lūgti, Guest!";
+    } else {
+        var name = getCookie("name");
+        var surname = getCookie("surname");
+        var color = getCookie("color");
+
+        if (name && surname && color) {
+            document.body.style.backgroundColor = color;
+            document.getElementById("welcomeMessage").textContent = "Laipni lūgti, " + name + " " + surname + "!";
+        }
+    }
+}
